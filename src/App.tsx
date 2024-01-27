340a20b36fd95a327928ed16a80a00e4147000b8
jusish <ishimwejustin67@gmail.com>
2024-01-27 11:53:35 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="justify-center items-center flex flex-col">
+    <div className="justify-center items-center flex flex-col px-10 py-20  bg-blue-600 text-white max-w-[450px] gap-10 rounded-lg lg:m-10 md:m-5 m-2 hover:bg-gradient-to-t from-purple-500 to-blue-400">
       <div>
-        <p>Count is {count}</p>
+        <p className="text-4xl">Count is {count}</p>
       </div>
-      <div className="flex flex-row gap-10">
-        <button onClick={() => setCount(count - 1)}>Decrement count</button>
-        <button onClick={() => setCount(count + 1)}>Increment count</button>
+      <div className="flex flex-row gap-5">
+        <button
+          onClick={() => setCount(count - 1)}
+          className="bg-gradient-to-r from-blue-400 to-purple-600 px-5 py-3 rounded-full"
+        >
+          Decrement count
+        </button>
+        <button
+          onClick={() => setCount(count + 1)}
+          className=" bg-gradient-to-r from-green-300 to-purple-600 px-5 py-3 rounded-full"
+        >
+          Increment count
+        </button>
       </div>
     </div>
   );
